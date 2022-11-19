88c025465f15427074cffe36653ef21a9f339425
aneez004 <aneez004@gmail.com>
2022-11-19 16:35:09 +0530

    added needrestart related conf

--- k8s_master_gce-v1.24.4.sh
+++ k8s_master_gce-v1.24.4.sh
@@ -1,4 +1,5 @@
-﻿apt-get update
+echo "\$nrconf{restart} = 'a';" | sudo tee -a /etc/needrestart/needrestart.conf
+apt-get update
 apt-get install apt-transport-https ca-certificates curl gnupg-agent software-properties-common -y      
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg |  gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
 echo "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" |  tee /etc/apt/sources.list.d/docker.list > /dev/null
