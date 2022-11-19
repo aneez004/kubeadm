20708af18c25c782adb02098f4e518ebec6140ac
Anees A A <aneesa@cloudthat.com>
2022-11-19 17:42:36 +0530

    modified master creation script

--- k8s_master_gce-v1.24.4.sh
+++ k8s_master_gce-v1.24.4.sh
@@ -5,8 +5,8 @@ apt-get update
 apt-get install apt-transport-https ca-certificates curl gnupg-agent software-properties-common -y      
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg |  gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
 echo "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" |  tee /etc/apt/sources.list.d/docker.list > /dev/null
- apt-get update -y
- apt-get install -y containerd.io docker-ce-cli
+apt-get update -y
+apt-get install -y containerd.io docker-ce-cli
 
 
 #Configure iptables
@@ -16,27 +16,27 @@ overlay
 br_netfilter
 EOF
 
- modprobe overlay
- modprobe br_netfilter
+modprobe overlay
+modprobe br_netfilter
 
 cat <<EOF |  tee /etc/sysctl.d/k8s.conf
 net.bridge.bridge-nf-call-ip6tables = 1
 net.bridge.bridge-nf-call-iptables = 1
 net.ipv4.ip_forward = 1
 EOF
- sysctl --system
+sysctl --system
 
 #Add the Kubernetes apt repository and public signing key
- curl -fsSLo /usr/share/keyrings/kubernetes-archive-keyring.gpg https://packages.cloud.google.com/apt/doc/apt-key.gpg
+curl -fsSLo /usr/share/keyrings/kubernetes-archive-keyring.gpg https://packages.cloud.google.com/apt/doc/apt-key.gpg
 
 echo 'deb [signed-by=/usr/share/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main' |  tee /etc/apt/sources.list.d/kubernetes.list
 
 # Install kubelet kubectl and kubeadm
 
 
- apt-get update
- apt-get install -y kubelet=1.24.4-00 kubeadm=1.24.4-00 kubectl=1.24.4-00
- apt-mark hold kubelet kubeadm kubectl
+apt-get update
+apt-get install -y kubelet=1.24.4-00 kubeadm=1.24.4-00 kubectl=1.24.4-00
+apt-mark hold kubelet kubeadm kubectl
 
 # Disable swap memory
 
@@ -45,18 +45,18 @@ sed -i '/ swap / s/^\(.*\)$/#\1/g' /etc/fstab
 
 #Enable and restart the services containerd and kubelet
 
- systemctl enable kubelet
- systemctl restart kubelet
+systemctl enable kubelet
+systemctl restart kubelet
 #rm /etc/containerd/config.toml
 containerd config default |  tee /etc/containerd/config.toml >/dev/null 2>&1
 sed -i 's/SystemdCgroup \= false/SystemdCgroup \= true/g' /etc/containerd/config.toml
- systemctl restart containerd
- systemctl enable containerd
- systemctl daemon-reload
+systemctl restart containerd
+systemctl enable containerd
+systemctl daemon-reload
 
 # Set hostname as private ipv4 dnsname from the instance metadata
 
- hostnamectl set-hostname $(curl  "http://metadata.google.internal/computeMetadata/v1/instance/hostname" -H "Metadata-Flavor: Google")
+hostnamectl set-hostname $(curl  "http://metadata.google.internal/computeMetadata/v1/instance/hostname" -H "Metadata-Flavor: Google")
 
 #cluster_name = $1
 
@@ -92,7 +92,7 @@ EOF
 
 
 # Migrate kubeconfig to a version compatible with the current kubeadm version
- kubeadm config migrate --old-config /tmp/kubeconfigold.yaml --new-config /tmp/kubeconfig.yaml
+kubeadm config migrate --old-config /tmp/kubeconfigold.yaml --new-config /tmp/kubeconfig.yaml
 export HOME=/root
 # Creates the kubernetes cluster using the config file
 kubeadm init --config /tmp/kubeconfig.yaml
