3d44f39c7ab3e45952798a5fb903ab399e4c80ae
Anees A A <aneesa@cloudthat.com>
2022-11-20 18:40:38 +0530

    dos2unix formatted worker script

--- k8s_worker_gce_v1.24.4.sh
+++ k8s_worker_gce_v1.24.4.sh
@@ -1,4 +1,6 @@
-apt-get update
+#!/bin/bash
+echo "\$nrconf{restart} = 'a';" | sudo tee -a /etc/needrestart/needrestart.conf
+apt update -y
 apt-get install apt-transport-https ca-certificates curl gnupg-agent software-properties-common -y      
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
 echo "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
